ec519050105086dc9e3e0488f3e9aa9dd80d1e7a
beavernee <tkddnd331@naver.com>
2014-12-12 12:41:23 +0900

    2014-12-12_12:41PM

---
+++ Assets/TeST.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeST : MonoBehaviour {
+    public AudioSource _Stage1;
+    public AudioSource _Stage2;
+    private bool Test = false;
+	// Use this for initialization
+	void Start () {
+        _Stage1.Play();
+        _Stage2.Stop();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.A))
+        {
+            Test = true;
+        }
+
+        if(Test = true)
+        {
+            _Stage1.Pause();
+            _Stage2.Play();
+        }
+	}
+}
